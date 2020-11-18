4ee115eefad293164fc67d7fbb5e4b451efc3eea
unknown <sadegh.pouriyan.zadeh.95@gmail.com>
2020-11-18 22:40:23 +0330

    calculation for unknown number of resistors added

--- electricpy/resistance.py
+++ electricpy/resistance.py
@@ -1,30 +1,32 @@
-# TODO: make function calculate on unknown number of resistances
-def equivalent_parallel_resistance(r_1: float, r_2: float) -> float:
-	""" Calculate equivalent resistance.
+# TODO:
+def equivalent_resistance(resistance_list: list, parallel=None) -> float:
+    """The total resistance for resistors fully connected in series or parallel
 
-	Args:
-		r_1 (float): first resistance
-		r_2 (float): second resistance
+    Parameters
+    ----------
+    resistance_list : list
+        list of resistors fully connected in series or parallel
+    
+    Returns
+    -------
+    float
+        The total resistance for resistors fully connected in series or parallel
+    """
+    assert parallel != None, "No Connection Selected" # no connection type is selected
 
-	Returns:
-		float: equivalent resistance
-	"""	
-	eq_r = (r_1**-1 + r_2**-1)**-1
-	return eq_r
+    if parallel:
+        equivalent_resistance = 0
+        for i in range(len(resistance_list)):
+            equivalent_resistance += 1/(resistance_list[i])
+        equivalent_resistance = 1/equivalent_resistance
 
+    if not parallel:
+        equivalent_resistance = 0
+        for i in range(len(resistance_list)):
+            equivalent_resistance += resistance_list[i]
 
-# TODO: make function calculate on unknown number of resistances
-def equivalent_serial_resistance(r_1: float, r_2: float) -> float:
-	""" Calculate serial resistance.
+    return equivalent_resistance
 
-	Args:
-		r_1 (float): first resistance
-		r_2 (float): second resistance
+res_list = [5, 7]
 
-	Returns:
-		float: equivalent resistance
-	"""	
-	eq_r = r_1 + r_2
-	return eq_r
-
-
+print(equivalent_resistance(res_list, False))
